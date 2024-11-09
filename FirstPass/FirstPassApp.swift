d413b4f72c615cb6c91965b16b853518d6d0f18f
Vinícius Couto Tasso <vcoutasso@gmail.com>
2024-11-08 22:54:03 -0300

    chore: create xcode project

---
+++ FirstPass/FirstPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstPassApp.swift
+//  FirstPass
+//
+//  Created by Vinícius Couto on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstPassTests/FirstPassTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstPassTests.swift
+//  FirstPassTests
+//
+//  Created by Vinícius Couto on 08/11/24.
+//
+
+import Testing
+@testable import FirstPass
+
+struct FirstPassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
